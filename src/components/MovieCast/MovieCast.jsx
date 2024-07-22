145458c5ac5e9ccc0eb7e67e1f7b4ac8541b516e
iahub4metra <buhai.artem7@gmail.com>
2024-07-22 21:25:55 +0100

    swiper cast

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -3,6 +3,11 @@ import getCast from "../../js/movieCastRequest";
 import { useLocation } from "react-router-dom";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import css from "./MovieCast.module.css"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import "./swiperCustomCast.css"
+import { Navigation, Pagination, EffectCoverflow } from 'swiper/modules';
+import { IoArrowForwardOutline, IoArrowBackOutline } from "react-icons/io5";
 const defaultImg = 'https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg'
 
 const MovieCast = ({baseUrlProfile}) => {
@@ -32,15 +37,49 @@ const MovieCast = ({baseUrlProfile}) => {
     return ( 
         <>  
             {showErrorMsg && <ErrorMessage errorType={errorType} />}
-            {cast && <ul className={css.castList}>
-                {cast.map((actor) => (
-                    <li key={actor.id} className={css.castItem}>
-                        <img className={css.actorImage} src={actor.profile_path ? `${baseUrlProfile}${actor.profile_path}` : defaultImg} alt={actor.name} width={185}/>
-                        <h3 className={css.actorName}>{actor.name}</h3>
-                        <p className={css.actorCharacter}>{actor.character}</p>
-                    </li>
-                ))}
-            </ul>}
+            {cast &&
+                <>
+                <Swiper
+                    className={css.castList}
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={'auto'}
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 1,
+                        slideShadows: true,
+                    }}
+                    pagination={true}
+                    modules={[EffectCoverflow, Pagination, Navigation]}
+                    navigation={{
+                                nextEl: '.custom-swiper-button-next',
+                                prevEl: '.custom-swiper-button-prev',
+                            }}
+                >
+                        {cast.map((actor) => (
+                        <SwiperSlide>
+                            <li key={actor.id} className={css.castItem}>
+                                <img className={css.actorImage} src={actor.profile_path ? `${baseUrlProfile}${actor.profile_path}` : defaultImg} alt={actor.name} width={185}/>
+                                <h3 className={css.actorName}>{actor.name}</h3>
+                                <p className={css.actorCharacter}>{actor.character}</p>
+                            </li>   
+                        </SwiperSlide>
+                    ))}
+                    </Swiper>
+                    <div className="btns-reviews">
+                        <button className="custom-swiper-button custom-swiper-button-prev">
+                            <IoArrowBackOutline size={20} />
+                        </button>
+                        <button className="custom-swiper-button custom-swiper-button-next">
+                            <IoArrowForwardOutline size={20} />
+                        </button>
+                    </div>
+                </>
+                
+            }
             
         </>
      );
--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -7,6 +7,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
 import "./swiperCustom.css"
 import { Navigation, Pagination } from 'swiper/modules';
+import { IoArrowForwardOutline, IoArrowBackOutline } from "react-icons/io5";
 
 const MovieReviews = () => {
     const location = useLocation()
@@ -33,23 +34,47 @@ const MovieReviews = () => {
             <div className={css.reviewsContainer}>
                 {showErrorMsg && <ErrorMessage errorType={errorType} />}
                 {reviews.length > 0 &&
-                (<Swiper
-                    modules={[Navigation, Pagination]}
-                    spaceBetween={50}
-                    slidesPerView={1}
-                    navigation
-                    pagination={{ clickable: true }}
-                    className={css.reviewsList}
-                >
-                {reviews.map((review) => (
-                        <SwiperSlide key={review.id}>
-                            <li className={css.reviewsItem}>
-                                <h3 className={css.reviewsAuthor}>{review.author}</h3>
-                                <p className={css.reviewsContent}>{review.content}</p>
-                            </li>
-                        </SwiperSlide>
-                    ))}
-                    </Swiper>)
+                (
+                <>
+                    <Swiper
+                        modules={[Navigation, Pagination]}
+                        spaceBetween={50}
+                        slidesPerView={1}
+                        breakpoints={{
+                            768: {
+                                slidesPerView: 1,
+                            },
+                            1024: {
+                                slidesPerView: 2,
+                            }
+                        }}
+                        navigation={{
+                                nextEl: '.custom-swiper-button-next',
+                                prevEl: '.custom-swiper-button-prev',
+                            }}
+                        pagination={{ clickable: true }}
+                        className={css.reviewsList}
+                    >
+                    {reviews.map((review) => (
+                            <SwiperSlide key={review.id}>
+                                <li className={css.reviewsItem}>
+                                    <h3 className={css.reviewsAuthor}>{review.author}</h3>
+                                    <p className={css.reviewsContent}>{review.content}</p>
+                                </li>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                    <div className="btns-reviews">
+                        <button className="custom-swiper-button custom-swiper-button-prev">
+                        <IoArrowBackOutline size={20} />
+                    </button>
+                    <button className="custom-swiper-button custom-swiper-button-next">
+                        <IoArrowForwardOutline size={20} />
+                    </button>
+                    </div>
+                    
+                </>
+                )
                     }
                 {(!reviews.length > 0 && showErrorMsg === false) && (<p>There are no reviews on this movie yet</p>)}
             </div>
